de5998a69fdb217d2e93a87c3a91ab79adb2d801
hasip <hasifpriyambudi13@gmail.com>
2024-02-21 00:39:08 +0700

    First Commit

---
+++ go.mod
@@ -0,0 +1,55 @@
+module scrap-fb
+
+go 1.21.0
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/antchfx/htmlquery v1.3.0 // indirect
+	github.com/antchfx/xmlquery v1.3.18 // indirect
+	github.com/antchfx/xpath v1.2.5 // indirect
+	github.com/chromedp/cdproto v0.0.0-20240214232516-ad4608604e9e // indirect
+	github.com/chromedp/chromedp v0.9.5 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/go-rod/rod v0.114.7 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.3.2 // indirect
+	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/gocolly/colly/v2 v2.1.0 // indirect
+	github.com/gofiber/fiber v1.14.6 // indirect
+	github.com/gofiber/fiber/v2 v2.52.0 // indirect
+	github.com/gofiber/template v1.8.3 // indirect
+	github.com/gofiber/template/html/v2 v2.1.1 // indirect
+	github.com/gofiber/utils v1.1.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/imroc/req v0.3.2 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/ysmood/fetchup v0.2.3 // indirect
+	github.com/ysmood/goob v0.4.0 // indirect
+	github.com/ysmood/got v0.34.1 // indirect
+	github.com/ysmood/gson v0.7.3 // indirect
+	github.com/ysmood/leakless v0.8.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"github.com/chromedp/cdproto/network"
+	"github.com/chromedp/chromedp"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
+
+func main() {
+	// Create a new engine
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Get("/scrap/:url", func(c *fiber.Ctx) error {
+
+		url := c.Params("url")
+
+		// READ COOKIE
+		filePath := "cookie.txt"
+		content, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			return c.SendString("Error reading cookies")
+		}
+		cookieString := string(content)
+		// END READ COOKIE
+
+		htmlContent, err := fetchHTML("https://www.facebook.com/watch/?v="+url, cookieString)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c.Set("Content-Type", "application/json")
+		return c.JSON(htmlContent)
+	})
+
+	app.Get("/upload", func(c *fiber.Ctx) error {
+		return c.Render("upload", fiber.Map{
+			"Title": "Upload Kebenaran",
+		})
+	})
+
+	app.Post("/upload-proses", func(c *fiber.Ctx) error {
+		// Parse the form file
+		file, err := c.FormFile("file")
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		// Save the uploaded file to the server
+		newFile := "cookie.txt"
+		err = c.SaveFile(file, fmt.Sprintf("./%s", newFile))
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		return c.JSON(fiber.Map{
+			"message": "File uploaded successfully",
+		})
+	})
+
+	app.Listen(":5000")
+}
+
+func SetCookie(name, value, domain, path string, httpOnly, secure bool) chromedp.Action {
+	return chromedp.ActionFunc(func(ctx context.Context) error {
+		// expr := cdp.TimeSinceEpoch(time.Now().Add(180 * 24 * time.Hour))
+		network.SetCookie(name, value).
+			// WithExpires(&expr).
+			WithDomain(domain).
+			WithPath(path).
+			WithHTTPOnly(httpOnly).
+			WithSecure(secure).
+			Do(ctx)
+		return nil
+	})
+}
+
+func fetchHTML(url string, cookieString string) (map[string][]string, error) {
+	// Initialize a new browser context
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	cookiesString := "sb=K90YZSDwtyztrt3t3Ikb3nAz; datr=K90YZZbuu9xmmkNpoN2WSGpX; c_user=100009705467505; ps_n=0; ps_l=0; fbl_cs=AhDIMta36cB1%2B2jleeEuhfmbGDgzbkxaQlRYR2EyTGxQK05qT3ZsbnRLSA; fbl_ci=1320958615211278; m_page_voice=100009705467505; i_user=61550559231213; dpr=2.0000000298023224; wd=1778x316; xs=19%3ANlaiS5MlKQItAw%3A2%3A1707962674%3A-1%3A11050%3A%3AAcV-XrTTWQjX_HhW575i2wcqvXmjQn-DafpILkEq3Q; fr=1OAfGIFa2rvfES9nS.AWVFlOYr40LGGCqxXHJZyOuEAas.BlzcZP..AAA.0.0.BlzcZP.AWViFRlTV_4; locale=en_US; fbl_st=101634769%3BT%3A28466407; wl_cbv=v2%3Bclient_version%3A2408%3Btimestamp%3A1707984470"
+
+	// Split the string into individual cookies
+	cookiesSlice := strings.Split(cookiesString, "; ")
+
+	// Loop through each cookie and set it
+	for _, cookie := range cookiesSlice {
+		parts := strings.SplitN(cookie, "=", 2)
+		if len(parts) == 2 {
+			name := parts[0]
+			value := parts[1]
+			err := chromedp.Run(ctx, SetCookie(name, value, ".facebook.com", "/", false, false))
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	// Navigate to the URL and fetch the rendered HTML
+	var htmlContent map[string][]string
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+		chromedp.Evaluate(`
+		(function() {
+			var scripts = Array.from(document.querySelectorAll('script[data-content-len]'));
+			var urls = { hd: [], sd: [] };
+			var urlFoundHd = false;
+			var urlFoundSd = false;
+	
+			function findUrlInObject(obj, hdKey, sdKey) {
+					for (var key in obj) {
+							if (obj.hasOwnProperty(key)) {
+									if (key === 'browser_native_hd_url') {
+											urls.hd.push(obj[key]);
+											urlFoundHd = true;
+									} else if (key === 'browser_native_sd_url') {
+											urls.sd.push(obj[key]);
+											urlFoundSd = true;
+									} else if (typeof obj[key] === 'object') {
+											findUrlInObject(obj[key], hdKey, sdKey);
+											if (urlFoundHd && urlFoundSd) {
+													break;
+											}
+									}
+
+									if(urlFoundHd && urlFoundSd){
+										break;
+									}
+							}
+					}
+			}
+	
+			outerLoop: for (var i = 0; i < scripts.length; i++) {
+					var script = scripts[i];
+					var contentLen = parseInt(script.getAttribute('data-content-len'));
+	
+					if (!isNaN(contentLen) && contentLen > 10000) {
+							var jsonContent = script.innerText;
+	
+							try {
+									var jsonData = JSON.parse(jsonContent);
+	
+									if (typeof jsonData === 'object') {
+											findUrlInObject(jsonData, 'hd_url', 'sd_url');
+											if (urlFound) {
+													break outerLoop;
+											}
+									}
+							} catch (error) {
+									console.error('Error parsing JSON:', error);
+							}
+					}
+			}
+	
+			return urls;
+	})();
+	
+		`, &htmlContent),
+	)
+	if err != nil {
+		return htmlContent, err
+	}
+
+	return htmlContent, nil
+}
